baf1fc067ae8afa3fff7b9fac115809f58cb4f74
Nolann Chobert <nolann.chobert@nxp.com>
2024-02-23 14:19:34 +0100

    Enable segmentation demo on i.MX93
    Bash segmentation updated to work on iMX93
    
    Signed-off-by: Nolann Chobert <nolann.chobert@nxp.com>

--- segmentation/example_segmentation_deeplab_v3_tflite.sh
+++ segmentation/example_segmentation_deeplab_v3_tflite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2022-2023 NXP
+# Copyright 2022-2024 NXP
 
 set -x
 
@@ -16,11 +16,30 @@ source "${BASEDIR}/segmentation/segmentation_utils.sh"
 
 setup_env
 
-# model and framework dependant variables 
+case "${GPU2D_API}" in
+  G2D)
+    # g2d-based
+    SCALE_FORMAT="RGBA"
+    ;;
+  PXP)
+    # pxp-based
+    SCALE_FORMAT="BGR"
+    ;;  
+  *)
+    # cpu-based
+    SCALE_FORMAT=""
+    ;;
+esac
+
+# model and framework dependant variables
 declare -A MODEL_BACKEND
+declare -A MODEL_BACKEND_NPU
+MODEL_BACKEND_NPU[IMX8MP]="${MODELS_DIR}/deeplabv3_mnv2_dm05_pascal_quant_uint8_float32.tflite"
+MODEL_BACKEND_NPU[IMX93]="${MODELS_DIR}/deeplabv3_mnv2_dm05_pascal_quant_uint8_float32_vela.tflite"
+
 MODEL_BACKEND[CPU]="${MODELS_DIR}/deeplabv3_mnv2_dm05_pascal.tflite"
 MODEL_BACKEND[GPU]="${MODELS_DIR}/deeplabv3_mnv2_dm05_pascal.tflite"
-MODEL_BACKEND[NPU]="${MODELS_DIR}/deeplabv3_mnv2_dm05_pascal_quant_uint8_float32.tflite"
+MODEL_BACKEND[NPU]=${MODEL_BACKEND_NPU[${IMX}]}
 MODEL=${MODEL_BACKEND[${BACKEND}]}
 
 MODEL_WIDTH=513
@@ -30,13 +49,18 @@ FRAMEWORK="tensorflow-lite"
 
 # tensor filter configuration
 FILTER_COMMON="tensor_filter framework=${FRAMEWORK} model=${MODEL}"
+
+declare -A FILTER_BACKEND_NPU
+FILTER_BACKEND_NPU[IMX8MP]=" custom=Delegate:External,ExtDelegateLib:libvx_delegate.so ! "
+FILTER_BACKEND_NPU[IMX93]=" custom=Delegate:External,ExtDelegateLib:libethosu_delegate.so ! "
+
 declare -A FILTER_BACKEND
 FILTER_BACKEND[CPU]="${FILTER_COMMON}"
 FILTER_BACKEND[CPU]+=" custom=Delegate:XNNPACK,NumThreads:$(nproc --all) !"
 FILTER_BACKEND[GPU]="${FILTER_COMMON}"
 FILTER_BACKEND[GPU]+=" custom=Delegate:External,ExtDelegateLib:libvx_delegate.so ! "
 FILTER_BACKEND[NPU]="${FILTER_COMMON}"
-FILTER_BACKEND[NPU]+=" custom=Delegate:External,ExtDelegateLib:libvx_delegate.so ! "
+FILTER_BACKEND[NPU]+=${FILTER_BACKEND_NPU[${IMX}]}
 TENSOR_FILTER=${FILTER_BACKEND[${BACKEND}]}
 
 # tensor preprocessing configuration: normalize video for float input models
--- segmentation/segmentation_utils.sh
+++ segmentation/segmentation_utils.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2022 NXP
+# Copyright 2022-2024 NXP
 
 # segmentation pipeline g2d accelerated for video rescaling
-# g2d video compositor not working for 513x513 - nnstreamer bug
+# video compositor elements are not working for 513x513 - nnstreamer bug
 # https://github.com/nnstreamer/nnstreamer/issues/3866
 # fall back to regular videomixer
+
 function gst_exec_segmentation {
+
+  # accelerated video scaling before inferencing
+  local VIDEO_SCALE=$(accelerated_video_scale_str ${MODEL_WIDTH} ${MODEL_HEIGHT} ${SCALE_FORMAT})
 
   gst-launch-1.0 \
     multifilesrc location="${IMAGES_DIR}/image%04d.jpg" loop=true caps=image/jpeg,framerate=1/2 ! \
-      jpegdec ! imxvideoconvert_g2d ! \
-      video/x-raw,width=${MODEL_WIDTH},height=${MODEL_HEIGHT},format=RGBA ! tee   name=t \
+      jpegdec ! \
+      ${VIDEO_SCALE} \
+      tee   name=t \
     t. ! queue name=thread-nn max-size-buffers=2 leaky=2 ! \
-      videoconvert ! video/x-raw,format=RGB ! tensor_converter ! \
+      videoconvert ! video/x-raw,format=RGB ! \
+      tensor_converter ! \
       ${TENSOR_PREPROCESS} \
       ${TENSOR_FILTER} \
       ${TENSOR_DECODER} \
